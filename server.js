1a96e83bca674e898377123e536345a41d07928e
Guggilla Raj Kumar <lucifergaming1947@gmail.com>
2023-03-27 15:56:52 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 
+//creating our web-server 🤣😇
 const app = express()
 
+//activating our web-server
 app.listen(3000,()=>console.log('web server activated and listening on 3000...'))
 
